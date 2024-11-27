a54946ca4b22d66f2be277e225fbb40f5f6afa24
Rob Beach <surfdemon@gmail.com>
2024-11-27 15:52:40 +0000

    Maint: start views for property

--- property/urls.py
+++ property/urls.py
@@ -2,7 +2,7 @@
 URL configuration for config project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,11 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from property.views import PropertyList
+from . import views
 
 urlpatterns = [
-    path('editor/', include('django_summernote.urls')),
-    path('admin/', admin.site.urls),
-    path('', include('property.urls')),
+    path('', PropertyList.as_view(), name='property_list'),
 ]
--- property/views.py
+++ property/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+"""
+    This file creates the views for the property app.
+
+    ''Views'': 
+        PropertyList: - Display a list of properties using the Property Model.
+        property_detail: - Display an individual property.
+"""
+from django.views import generic
+from django.db.models import Q
+from .models import Property
+
 
 # Create your views here.
+class PropertyList(generic.ListView):
+    """
+        Display a list of properties using the Property Model.
+        Based on the user's authentication status and property status.
+    """
+    queryset = Property.objects.filter(status=1)
+    template_name = 'property/index.html'
+    paginate_by = 6
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_authenticated:
+            return Property.objects.filter(
+                Q(status=1) | Q(status=0, owner=user)
+            )
+        else:
+            return Property.objects.filter(status=1)
+
